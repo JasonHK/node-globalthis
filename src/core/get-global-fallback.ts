97e415b7d31713fd60860ae00962d067c9589e09
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-23 19:02:31 +0800

    chore: added some comments

--- src/core/get-global-fallback.ts
+++ src/core/get-global-fallback.ts
@@ -6,18 +6,22 @@ export function getGlobalFallback(): typeof globalThis
 {
     if ((typeof self !== "undefined") && isObject(self))
     {
+        // The global `self` property, available in browser and Web Worker environment.
         return self;
     }
     else if ((typeof window !== "undefined") && isObject(window))
     {
+        // The global `window` property, available in browser environment.
         return window;
     }
     else if ((typeof global !== "undefined") && isObject(global))
     {
-        return global as unknown as typeof globalThis;
+        // The global `global` property, available in Node.js runtime environment.
+        return global;
     }
     else
     {
+        // Throws an `Error` when none of the known global properties exist.
         throw new Error("Unable to locate the global `this` object.");
     }
 }
