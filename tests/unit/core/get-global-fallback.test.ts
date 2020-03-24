156d6cb76685bc17c33565fa0178cc0c0846605d
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-24 15:26:41 +0800

    test: wrapped test suites with its file name

--- tests/unit/core/get-global-fallback.test.ts
+++ tests/unit/core/get-global-fallback.test.ts
@@ -6,66 +6,71 @@ const { after, afterEach, before, suite, test } = intern.getPlugin("interface.td
 const { assert } = intern.getPlugin("chai");
 
 suite(
-    "getGlobalFallback()",
+    "unit/core/get-global-fallback.test.ts",
     () =>
     {
-        const browserGlobalFake = {} as unknown as Window & typeof globalThis;
-        const nodeGlobalFake = {} as unknown as NodeJS.Global & typeof globalThis;
-
-        let globalReal: typeof global;
-
-        before(
+        suite(
+            "getGlobalFallback()",
             () =>
             {
-                // Store the real `global` property in a safe place, then delete the property.
-                globalReal = global;
-                delete global.global;
-            });
+                const browserGlobalFake = {} as unknown as Window & typeof globalThis;
+                const nodeGlobalFake = {} as unknown as NodeJS.Global & typeof globalThis;
         
-        afterEach(
-            () =>
-            {
-                // Delete the fake global properties if needed.
-                if ("global" in globalReal) { delete globalReal.global; }
-                if ("self" in globalReal) { delete globalReal.self; }
-                if ("window" in globalReal) { delete globalReal.window; }
-            });
+                let globalReal: typeof global;
         
-        after(
-            () =>
-            {
-                // Restore the real `global` property.
-                globalReal.global = globalReal;
-            });
-            
-        test(
-            "The global `global` property exists",
-            () =>
-            {
-                globalReal.global = nodeGlobalFake;
-                assert.strictEqual(getGlobalFallback(), nodeGlobalFake);
-            });
-
-        test(
-            "The global `self` property exists",
-            () =>
-            {
-                globalReal.self = browserGlobalFake;
-                assert.strictEqual(getGlobalFallback(), browserGlobalFake);
-            });
+                before(
+                    () =>
+                    {
+                        // Store the real `global` property in a safe place, then delete the property.
+                        globalReal = global;
+                        delete global.global;
+                    });
+                
+                afterEach(
+                    () =>
+                    {
+                        // Delete the fake global properties if needed.
+                        if ("global" in globalReal) { delete globalReal.global; }
+                        if ("self" in globalReal) { delete globalReal.self; }
+                        if ("window" in globalReal) { delete globalReal.window; }
+                    });
+                
+                after(
+                    () =>
+                    {
+                        // Restore the real `global` property.
+                        globalReal.global = globalReal;
+                    });
+                    
+                test(
+                    "The global `global` property exists",
+                    () =>
+                    {
+                        globalReal.global = nodeGlobalFake;
+                        assert.strictEqual(getGlobalFallback(), nodeGlobalFake);
+                    });
         
-        test(
-            "The global `window` property exists",
-            () =>
-            {
-                globalReal.window = browserGlobalFake;
-                assert.strictEqual(getGlobalFallback(), browserGlobalFake);
-            });
-
-        test(
-            "None of the known global properties exist",
-            () =>
-            {
-                assert.throws(() => { getGlobalFallback(); });
+                test(
+                    "The global `self` property exists",
+                    () =>
+                    {
+                        globalReal.self = browserGlobalFake;
+                        assert.strictEqual(getGlobalFallback(), browserGlobalFake);
+                    });
+                
+                test(
+                    "The global `window` property exists",
+                    () =>
+                    {
+                        globalReal.window = browserGlobalFake;
+                        assert.strictEqual(getGlobalFallback(), browserGlobalFake);
+                    });
+        
+                test(
+                    "None of the known global properties exist",
+                    () =>
+                    {
+                        assert.throws(() => { getGlobalFallback(); });
+                    });
             });
     });
--- tests/unit/core/get-global.test.ts
+++ tests/unit/core/get-global.test.ts
@@ -9,67 +9,72 @@ const { afterEach, suite, test } = intern.getPlugin("interface.tdd");
 const { assert } = intern.getPlugin("chai");
 
 suite(
-    "getGlobal()",
+    "unit/core/get-global.test.ts",
     () =>
     {
-        afterEach(
+        suite(
+            "getGlobal()",
             () =>
             {
-                Sinon.restore();
-            });
-
-        test(
-            "The global `__GLOBAL__` property refers to the global `this` value",
-            () =>
-            {
-                assert.strictEqual(getGlobal(), global);
-                assert.doesNotHaveAllKeys(Object.prototype, ["__GLOBAL__"]);
-            });
-
-        test(
-            "Failed to define `__GLOBAL__` property to `Object.prototype`",
-            () =>
-            {
-                const definePropertyReal = Object.defineProperty;
-
-                Sinon.stub(Object, "defineProperty")
-                    .callsFake(
-                        (target, key, descriptor) =>
-                        {
-                            if ((target === Object.prototype) && (key === "__GLOBAL__"))
-                            {
-                                throw new TypeError();
-                            }
-                            else
-                            {
-                                return definePropertyReal(target, key, descriptor);
-                            }
-                        });
-
-                const getGlobalFallbackMocked = Sinon.mock(GetGlobalFallback).expects("getGlobalFallback");
-                
-                getGlobal();
-                Sinon.assert.calledOnce(getGlobalFallbackMocked);
-            });
-
-        test(
-            "The global `__GLOBAL__` property was not found in the global scope",
-            () =>
-            {
-                Object.defineProperty(
-                    global,
-                    "__GLOBAL__",
+                afterEach(
+                    () =>
                     {
-                        get() { return undefined; },
-                        configurable: true,
+                        Sinon.restore();
                     });
-
-                const getGlobalFallbackMocked = Sinon.mock(GetGlobalFallback).expects("getGlobalFallback");
-
-                getGlobal();
-                Sinon.assert.calledOnce(getGlobalFallbackMocked);
-                assert.doesNotHaveAllKeys(Object.prototype, ["__GLOBAL__"]);
-
-                delete global.__GLOBAL__;
+        
+                test(
+                    "The global `__GLOBAL__` property refers to the global `this` value",
+                    () =>
+                    {
+                        assert.strictEqual(getGlobal(), global);
+                        assert.doesNotHaveAllKeys(Object.prototype, ["__GLOBAL__"]);
+                    });
+        
+                test(
+                    "Failed to define `__GLOBAL__` property to `Object.prototype`",
+                    () =>
+                    {
+                        const definePropertyReal = Object.defineProperty;
+        
+                        Sinon.stub(Object, "defineProperty")
+                            .callsFake(
+                                (target, key, descriptor) =>
+                                {
+                                    if ((target === Object.prototype) && (key === "__GLOBAL__"))
+                                    {
+                                        throw new TypeError();
+                                    }
+                                    else
+                                    {
+                                        return definePropertyReal(target, key, descriptor);
+                                    }
+                                });
+        
+                        const getGlobalFallbackMocked = Sinon.mock(GetGlobalFallback).expects("getGlobalFallback");
+                        
+                        getGlobal();
+                        Sinon.assert.calledOnce(getGlobalFallbackMocked);
+                    });
+        
+                test(
+                    "The global `__GLOBAL__` property was not found in the global scope",
+                    () =>
+                    {
+                        Object.defineProperty(
+                            global,
+                            "__GLOBAL__",
+                            {
+                                get() { return undefined; },
+                                configurable: true,
+                            });
+        
+                        const getGlobalFallbackMocked = Sinon.mock(GetGlobalFallback).expects("getGlobalFallback");
+        
+                        getGlobal();
+                        Sinon.assert.calledOnce(getGlobalFallbackMocked);
+                        assert.doesNotHaveAllKeys(Object.prototype, ["__GLOBAL__"]);
+        
+                        delete global.__GLOBAL__;
+                    });
             });
     });
