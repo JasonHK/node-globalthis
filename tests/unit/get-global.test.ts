3cd42e2b1997c301d4e5bfff2cef0dd338ce8264
Jason Kwok <JasonHK@users.noreply.github.com>
2020-03-24 15:30:20 +0800

    test: added tests for `getGlobal()`

---
+++ tests/unit/get-global.test.ts
@@ -0,0 +1,72 @@
+"use strict";
+
+import Sinon from "sinon";
+
+import { getGlobal } from "../../src/get-global";
+import * as GetGlobalInternal from "../../src/core/get-global";
+
+const { after, afterEach, before, suite, test } = intern.getPlugin("interface.tdd");
+const { assert } = intern.getPlugin("chai");
+
+suite(
+    "unit/get-global.test.ts",
+    () =>
+    {
+        suite(
+            "getGlobal()",
+            () =>
+            {
+                const globalThisFake = {};
+        
+                let globalThisExists = false;
+                let globalThisReal: typeof globalThis;
+        
+                before(
+                    () =>
+                    {
+                        if ("globalThis" in global)
+                        {
+                            globalThisExists = true;
+                            globalThisReal = global.globalThis;
+        
+                            // @ts-ignore
+                            delete global.globalThis;
+                        }
+                    });
+        
+                afterEach(
+                    () =>
+                    {
+                        Sinon.restore();
+        
+                        // @ts-ignore
+                        if ("globalThis" in global) { delete global.globalThis; }
+                    });
+        
+                after(
+                    () =>
+                    {
+                        // @ts-ignore
+                        if (globalThisExists) { global.globalThis = globalThisReal; }
+                    });
+        
+                test(
+                    "The global `globalThis` property exists",
+                    () =>
+                    {
+                        // @ts-ignore
+                        global.globalThis = globalThisFake;
+                        assert.strictEqual(getGlobal(), globalThisFake);
+                    });
+        
+                test(
+                    "The global `globalThis` property doesn't exist",
+                    () =>
+                    {
+                        const getGlobalInternalMocked = Sinon.mock(GetGlobalInternal).expects("getGlobal");
+
+                        getGlobal();
+                        Sinon.assert.calledOnce(getGlobalInternalMocked);
+                    });
+            });
+    });
