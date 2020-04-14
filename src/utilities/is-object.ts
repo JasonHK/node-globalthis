f67e858515b9ba0d60b2d8585f3a8843736bf640
Jason Kwok <JasonHK@users.noreply.github.com>
2020-04-14 22:20:01 +0800

    docs: added TSDoc

--- src/utilities/is-object.ts
+++ src/utilities/is-object.ts
@@ -1,5 +1,16 @@
 "use strict";
 
+/**
+ * Determine whether the payload is an object or not.
+ * 
+ * @remarks
+ * Functions will not be count as an object by this function.
+ * 
+ * @hidden
+ * 
+ * @param payload An value to be determined.
+ * @returns The assertion result.
+ */
 export function isObject(payload: unknown): boolean
 {
     return ((typeof payload === "object") && (payload !== null));
