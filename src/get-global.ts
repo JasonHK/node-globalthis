66fa312824195f4996a3611d98565876e9f45780
Jason Kwok <JasonHK@users.noreply.github.com>
2020-04-15 00:36:14 +0800

    docs: added TSDoc

--- src/get-global.ts
+++ src/get-global.ts
@@ -4,6 +4,16 @@ import { getGlobalInternal } from "./core/get-global-internal";
 
 import { isObject } from "./utilities/is-object";
 
+/**
+ * Retrieve the global `this` value.
+ * 
+ * The global `globalThis` property exists and it's an object, return it immediately. Otherwise, use
+ * the internal polyfill methods to try to retrieve the value.
+ * 
+ * @since 0.0.1
+ * 
+ * @returns The global `this` value.
+ */
 export function getGlobal(): typeof globalThis
 {
     if ((typeof globalThis !== "undefined") && isObject(globalThis))
