8461b889aeb612131e72829b4ec59fd826ad4597
Jason Kwok <JasonHK@users.noreply.github.com>
2020-04-15 14:10:42 +0800

    refactor: updated the polyfill function

--- src/apply-polyfill.ts
+++ src/apply-polyfill.ts
@@ -2,24 +2,34 @@
 
 import { globalThis as context } from "./globalthis";
 
+import { isObject } from "./utilities/is-object";
+
+/**
+ * Apply the `globalThis` polyfill to the global scope if the global `globalThis` property does not
+ * exist, or its attributes do not compile with the specification.
+ */
 export function applyPolyfill(): void
 {
-    if (!Object.prototype.hasOwnProperty.call(context, "globalThis") || (context.globalThis !== context))
+    const descriptor = Object.getOwnPropertyDescriptor(context, "globalThis");
+    if (isObject(descriptor))
     {
-        try
-        {
-            Object.defineProperty(
-                context,
-                "globalThis",
-                {
-                    value: context,
-                    configurable: true,
-                    writable: true,
-                });
-        }
-        catch
-        {
-            throw new Error("Failed to apply the global `globalThis` property polyfill.");
-        }
+        const { configurable, enumerable, value, writable } = descriptor;
+        if (configurable && writable && !enumerable && (value === context)) { return; }
+    }
+
+    try
+    {
+        Object.defineProperty(
+            context,
+            "globalThis",
+            {
+                value: context,
+                configurable: true,
+                writable: true,
+            });
+    }
+    catch
+    {
+        throw new Error("Failed to apply the global `globalThis` property polyfill.");
     }
 }
