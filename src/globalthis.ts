d53f37806044c186a2c10c7bfb1e4cd729bd9b3a
Jason Kwok <JasonHK@users.noreply.github.com>
2020-04-16 14:33:56 +0800

    docs: added TSDoc

--- src/globalthis.ts
+++ src/globalthis.ts
@@ -4,4 +4,9 @@ import { getGlobal } from "./get-global";
 
 import { IGlobalThis } from "./interfaces/globalthis";
 
+/**
+ * The global `this` value.
+ * 
+ * @since 0.0.1
+ */
 export const globalThis: IGlobalThis = getGlobal();
