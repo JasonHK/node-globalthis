bdd84c896b0e3e5795c2a45575694e133eb4616e
Jason Kwok <JasonHK@users.noreply.github.com>
2020-04-16 14:22:12 +0800

    docs: added TSDoc

--- src/interfaces/globalthis.ts
+++ src/interfaces/globalthis.ts
@@ -1,3 +1,12 @@
 "use strict";
 
+/**
+ * Representing the global `this` value.
+ * 
+ * @remarks
+ * This type alias was required since the type of a variable named `globalThis` cannot be set as
+ * `typeof globalThis` directly.
+ * 
+ * @since 0.0.1
+ */
 export type IGlobalThis = typeof globalThis;
