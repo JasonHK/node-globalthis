4e57db648b9c0ccdc7d26e7005b03138e550d60c
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-21 15:46:49 +0800

    docs: removed some tags

--- src/utilities/is-object.ts
+++ src/utilities/is-object.ts
@@ -3,11 +3,7 @@
 /**
  * Determine whether the payload is an object or not.
  * 
- * @notes
- * Functions will not be count as an object by this function.
- * 
  * @internal
- * @since 0.0.1
  * 
  * @param payload An value to be determined.
  * @returns The assertion result.
