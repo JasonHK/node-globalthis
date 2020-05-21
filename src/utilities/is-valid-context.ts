69039b070848665806920e7e3561c2bb69e5ac1f
Jason Kwok <JasonHK@users.noreply.github.com>
2020-05-21 15:45:37 +0800

    chore: added function `isValidContext`

---
+++ src/utilities/is-valid-context.ts
@@ -0,0 +1,31 @@
+import type { IGlobalThis } from "../interfaces/globalthis";
+
+import { isObject } from "./is-object";
+
+/**
+ * Determine whether the payload is a valid `globalThis` value or not.
+ * 
+ * @internal
+ * 
+ * @param payload An value to be determined.
+ * @returns The assertion result.
+ */
+export function isValidContext(payload: unknown): payload is IGlobalThis
+{
+    // Return `false` if the payload is not an object.
+    if (!isObject(payload)) { return false; }
+
+    // Retrieve the descriptor of `globalThis` property in the payload. Then determine whether the
+    // property exists or not (the value is not `undefined`).
+    const descriptor = Object.getOwnPropertyDescriptor(payload, "globalThis");
+    if (isObject(descriptor))
+    {
+        // If the property exists, retrieve the attributes of it. Return `true` if its attributes compile
+        // with the specification, return `false` otherwise.
+        const { configurable, enumerable, value, writable } = descriptor;
+        return !!(configurable && writable && !enumerable && (value === payload));
+    }
+    
+    // Return `false` if the property does not exist.
+    return false;
+}
